6ec1c9ba46c1d4562805362d3ac71b51eb6e5f5a
ja10th <oluwaleyejames@gmail.com>
2022-10-03 01:34:03 +0100

    env changes

--- next.config.js
+++ next.config.js
@@ -2,15 +2,16 @@
 const nextConfig = {
   reactStrictMode: true,
   swcMinify: true,
-}
+};
 
 module.exports = {
   webpack: (config, { buildId, dev, isServer, defaultLoaders, webpack }) => {
     // Note: we provide webpack above so you should not `require` it
     // Perform customizations to webpack config
-    config.plugins.push(new webpack.IgnorePlugin(/\/__tests__\//))
+    config.plugins.push(new webpack.IgnorePlugin(/\/__tests__\//));
 
     // Important: return the modified config
-    return config
+    return config;
   },
-}
+  nextConfig,
+};
