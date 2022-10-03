5a1c96e2247c1a203d12967dc9cb5113483d636f
ja10th <oluwaleyejames@gmail.com>
2022-10-03 01:35:53 +0100

    env changes

--- next.config.js
+++ next.config.js
@@ -5,13 +5,14 @@ const nextConfig = {
 };
 
 module.exports = {
-  webpack: (config, { buildId, dev, isServer, defaultLoaders, webpack }) => {
-    // Note: we provide webpack above so you should not `require` it
-    // Perform customizations to webpack config
-    config.plugins.push(new webpack.IgnorePlugin(/\/__tests__\//));
-
-    // Important: return the modified config
-    return config;
-  },
   nextConfig,
+  future: {
+    webpack5: true,
+  },
+  webpack: (config, {}) => {
+    config.resolve.fallback = {
+      ...config.resolve.fallback,
+      fs: false,
+    };
+  },
 };
