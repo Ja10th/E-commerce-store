dfec54d0644bb6f495e9cc15283775e9644eda13
ja10th <oluwaleyejames@gmail.com>
2022-10-03 01:43:22 +0100

    env changes

--- next.config.js
+++ next.config.js
@@ -1,18 +1,6 @@
 /** @type {import('next').NextConfig} */
 const nextConfig = {
   reactStrictMode: true,
-  swcMinify: true,
-};
+}
 
-module.exports = {
-  nextConfig,
-  future: {
-    webpack5: true,
-  },
-  webpack: (config, {}) => {
-    config.resolve.fallback = {
-      ...config.resolve.fallback,
-      fs: false,
-    };
-  },
-};
+module.exports = nextConfig
